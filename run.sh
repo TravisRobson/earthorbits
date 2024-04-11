63cb3a262318efb5728122275bd8c70f0ae5a976
Travis Robson <travisjrobson@gmail.com>
2024-04-10 20:00:56 -0700

    add clang analyzer checks via scan-build

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir build
-cmake -S . -B build \
+scan-build cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=OFF \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DEOB_ENABLE_CLANG_TIDY=OFF \
     -DEOB_COMPILE_WARNINGS_AS_ERRORS=ON
-cmake --build build
+scan-build --view cmake --build build -j 6
